c2dc1c3564b8c78bc95ea75d4ac730145fc9426b
Tomo Suzuki <tomo@soultrust.com>
2025-01-15 21:53:37 -0500

    edit access control allow credentials

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-credentials", false);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
